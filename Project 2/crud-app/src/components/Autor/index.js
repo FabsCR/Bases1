0c0bccf1a5b90dd8c268ccdc97d181319b694657
Fabián Fernández <fabianfdez06@gmail.com>
2024-06-02 00:00:30 -0600

    feat: autor index

---
+++ Project 2/crud-app/src/components/Autor/index.js
@@ -0,0 +1,4 @@
+export { default as CreateAutor } from './CreateAutor';
+export { default as DeleteAutor } from './DeleteAutor';
+export { default as UpdateAutor } from './UpdateAutor';
+export { default as ReadAutor } from './ReadAutor';
